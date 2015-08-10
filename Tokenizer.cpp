11913de1c9ad21b6ca59c0b4dd3c82d89a52dcbb
SumeetSinha <sumeet.kumar507@gmail.com>
2015-08-10 09:37:32 -0700

    Fixed startupps mode

--- Tokenizer.cpp
+++ Tokenizer.cpp
@@ -20,6 +20,8 @@ Tokenizer::Tokenizer(){
 	Token="";
 	strDelim="";
 	currPos=inpString.begin();
+	mode = 0;
+	nofTokens=0;
 }
 
 Tokenizer::Tokenizer(const string& str, const string& delm){
@@ -28,6 +30,8 @@ Tokenizer::Tokenizer(const string& str, const string& delm){
 	Token="";
 	strDelim=delm;
 	currPos=inpString.begin();
+	mode = 0;
+	nofTokens=0;
 }
 
 Tokenizer::Tokenizer(vector<string>& strList, const string& str, const string& delm){
@@ -37,6 +41,8 @@ Tokenizer::Tokenizer(vector<string>& strList, const string& str, const string& d
 	strDelim=delm;
 	strList= this->split();
 	currPos=inpString.begin();
+	mode = 0;
+	nofTokens=0;
 }
 
 Tokenizer::~Tokenizer(){}
@@ -50,12 +56,16 @@ void Tokenizer::set(const string& str, const string& delm){
 	this->inpString=str;
 	this->strDelim=delm;
 	this->currPos=this->inpString.begin();	
+	mode = 0;
+	nofTokens=0;
 }
 
 void Tokenizer::setString(const string& str){
 
 	this->inpString=str;
 	this->currPos=this->inpString.begin();
+	mode = 0;
+	nofTokens=0;
 }
 
 void Tokenizer::setDelimiter(const string& delm){
